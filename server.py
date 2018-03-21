286645e95b609c263a3f6d07fb477a74b2ab9fd6
Phillip <p.j.wozny@students.uu.nl>
2018-03-21 13:23:50 +0100

    changed init params

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ num_cities = 1
 
 shock_duration = UserSettableParameter('slider', "Shock Duration", 100, 0, 600, 20)
 shock_period = UserSettableParameter('slider', "Shock Period", 200, 0, 600, 20)
-shock_rate = UserSettableParameter('slider', "Shock Growth", .5, 0, 1, .01)
+shock_rate = UserSettableParameter('slider', "Shock Growth", .2, 0, 1, .01)
 
 
 nc_rate = UserSettableParameter('slider', "In-Flow", .8, 0, 1, .1)
