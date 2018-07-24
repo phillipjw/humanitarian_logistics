dec64e9a89dd6dcc751545fbce77ee69d68d2587
rossgore <ross.gore@gmail.com>
2018-07-24 09:18:50 -0400

    Updating for cultural well being comparison

--- model_explorer.py
+++ model_explorer.py
@@ -9,7 +9,9 @@ import traceback
 
 class ModelExplorer():
 
-    def __init__(self, p_width=200, p_height=200, p_num_pols=2, p_city_size=20, p_number_steps=500, p_shock_flag =False, p_decision_quality=True):
+    def __init__(self, p_width=200, p_height=200, p_num_pols=2, p_city_size=20, p_number_steps=500, p_shock_flag =False, p_decision_quality=True, 
+                 p_coa_se=55, p_coa_st=65, p_coa_c=45, p_coa_otc=60, p_ngo_se=45, p_ngo_st=50, p_ngo_c=55, p_ngo_otc=48, p_ind_se=45, p_ind_st=50, p_ind_c=55, p_ind_otc=65,
+                 p_cultural_wellbeing=True, p_cw_group='A'):
 
         self.width = p_width
         self.height = p_height
@@ -18,7 +20,20 @@ class ModelExplorer():
         self.number_steps = p_number_steps
         self.shock_flag = p_shock_flag
         self.dq = p_decision_quality
-            
+        self.coa_se = p_coa_se
+        self.coa_st = p_coa_st
+        self.coa_c = p_coa_c
+        self.coa_otc = p_coa_otc
+        self.ngo_se = p_ngo_se
+        self.ngo_st = p_ngo_st
+        self.ngo_c = p_ngo_c
+        self.ngo_otc = p_ngo_otc
+        self.ind_se = p_ind_se
+        self.ind_st = p_ind_st
+        self.ind_c = p_ind_c
+        self.ind_otc = p_ind_otc
+        self.cultural_wellbeing = p_cultural_wellbeing
+        self.cw_group = p_cw_group
     
     def sweep(self, exp_all_time_steps, coa_se, coa_st, coa_c, coa_otc, ngo_se, ngo_st, ngo_c, ngo_otc, ind_se, ind_st, ind_c, ind_otc):
         
@@ -592,3 +607,86 @@ class ModelExplorer():
         except Exception as e:
             print(str(e))
             return None
+
+    def trace_given_cw(self):
+        
+        
+        myData = [["step", "coa_se_value", "coa_st_value", "coa_c_value", "coa_otc_value", "ngo_se_value", "ngo_st_value", "ngo_c_value", "ngo_otc_value", "ind_se_value", "ind_st_value", "ind_c_value", "ind_otc_value", "ngo_funds", "ngo_cfr", "ngo_cme", "city_po", "building_health", "newcomer_acculturation", "azc_staff", "city_costs", "city_crime", "coa_costs", "newcomer_health", "newcomer_distress", "cw_group"]]
+        
+        fileprefix = 'trace-of-cw-'
+        filesuffix = str(self.cw_group) + '.csv'
+        
+        filename = fileprefix + filesuffix
+        myFile = open(filename, 'w')
+        with myFile:
+            writer = csv.writer(myFile)
+            writer.writerows(myData)
+        
+        sim_values = self.trace(all_time_steps=True)
+        if sim_values is not None:
+            for x in range(0, len(sim_values)):
+                valuesToAdd = sim_values[x]
+                if valuesToAdd is not None:
+                    myFile = open(filename, 'a')
+                    with myFile:
+                        writer = csv.writer(myFile)
+                        writer.writerows([valuesToAdd])
+        if sim_values is None:
+            print("Exception thrown during simulation run.")
+    
+    def trace(self, all_time_steps):
+        toReturn = []
+        test = HumanitarianLogistics(False, self.width, self.height, self.num_pols, self.city_size, self.coa_se, self.coa_st, self.coa_c, self.coa_otc,
+                                     self.ngo_se, self.ngo_st, self.ngo_c, self.ngo_otc, self.ind_se, self.ind_st, self.ind_c, self.ind_otc)
+        test.shock_flag = self.shock_flag
+        test.dq = self.dq
+        test.include_social_networks = False
+        test.cultural_wellbeing = self.cultural_wellbeing
+        test.cw_group = self.cw_group
+            
+        ngo = [x for x in test.schedule.agents if type(x) is NGO]
+        coa_array = [coa for coa in test.schedule.agents if type(coa) is COA and coa.city.modality == 'AZC']
+            
+        inds = [ind for ind in test.schedule.agents if type(ind) is IND]
+        azcs = [azc for azc in test.schedule.agents if type(azc) is AZC]
+        cities = []
+        for coa in coa_array:
+            if coa.city is not None:
+                cities.append(coa.city)
+        for step in range(0,self.number_steps):
+            test.step()
+            buf_coa_cost = []
+            for ind_unit in inds:
+                buf_coa_cost.append([ind_unit.city.coa.housing_costs + ind_unit.city.coa.hotel_costs])
+            coa_costs = np.nansum(buf_coa_cost)
+            buf = []
+            for coa in coa_array:
+                for building in coa.city.azcs:
+                    if (np.isnan(building.health)==False):
+                        buf.append(building.health)
+            bh = np.mean(buf)
+            #bh = np.nanmean(buf)
+            buf_ac = []
+            buf_health = []
+            buf_distress = []
+            newcomers = [nc for nc in test.schedule.agents if type(nc) is Newcomer and nc.ls == 'as_ext']
+            for nc in newcomers:
+                buf_ac.append(nc.acculturation)
+                buf_health.append(nc.health)
+                buf_distress.append(nc.values.health)
+            ac = np.nanmean(buf_ac)
+            nc_health = np.nanmean(buf_health)
+            nc_distress = np.nanmean(buf_distress)
+            ng = np.nanmean([ngo.funds for ngo in ngo])
+            ngo_cfr = np.nanmean([ngo.cumulative_funds_raised for ngo in ngo])
+            ngo_cme = np.nanmean([ngo.cumulative_marketing_expenditures for ngo in ngo])
+            ct = np.nanmean([city.public_opinion for city in cities])
+            ct_costs = np.nanmean([city.costs for city in cities])
+            ct_crime = np.nanmean([city.crime for city in cities])
+            staff = np.nanmean([coa.staff for coa in coa_array])
+            values = [step, self.coa_se, self.coa_st, self.coa_c, self.coa_otc, self.ngo_se, self.ngo_st, self.ngo_c, self.ngo_otc, self.ind_se, self.ind_st, self.ind_c, self.ind_otc, ng, ngo_cfr, ngo_cme, ct, bh, ac, staff, ct_costs, ct_crime, coa_costs, nc_health, nc_distress, self.cw_group]
+            if all_time_steps==True:
+                toReturn.append(values)
+        if all_time_steps == False:
+            toReturn.append(values)
+        return (toReturn)
